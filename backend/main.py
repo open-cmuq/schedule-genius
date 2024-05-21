ffa56a56fdafe2ad1bdc77f034081d9957a74f2a
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2024-05-21 23:05:14 +0300

    Need to restructure modules and make JSON conversions friendly, need to remove unnecessary code from read_infosilem

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from pathlib import Path
 import json
 import os
+import mimetypes
+import importlib
+
+# Load the Excel utils, this isn't the most ideal code
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils/trial-formatter/trial-formatter.py'))
+
+# Get the module name
+module_name = os.path.splitext(os.path.basename(module_path))[0]
+
+# Load the module
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+module = importlib.util.module_from_spec(spec)
 
 app = FastAPI()
 
@@ -43,3 +55,14 @@ async def getAudit(major, entry_year):
     except:
         raise HTTPException(status_code=500, detail="Audit file note found")
 
+
+@app.post("/upload")
+async def upload_schedule(file: UploadFile = File(...)):
+    mime_type,_ = mimetypes.guess_type(file.filename)
+    # User input is one of the trickiest things, the least we can do is 
+    # verify that any uploaded documents are excel sheets and that 
+    # code injection shouldn't be possible
+    if mime_type != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        raise HTTPException(status_code=400, detail="Invalid file type. Only Excel files are allowed.")
+    
+    return {"NotImplemented": True}
