100befaa27efe9c499fbf055ea1506bc216d644d
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2024-05-22 18:01:12 +0300

    Sveltekit time

---
+++ frontend/.svelte-kit/ambient.d.ts
@@ -0,0 +1,257 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const SHELL: string;
+	export const npm_command: string;
+	export const npm_config_userconfig: string;
+	export const COLORTERM: string;
+	export const HYPRLAND_CMD: string;
+	export const npm_config_cache: string;
+	export const npm_package_dev_optional: string;
+	export const WLR_NO_HARDWARE_CURSORS: string;
+	export const CONDA_EXE: string;
+	export const _CE_M: string;
+	export const XDG_BACKEND: string;
+	export const npm_package_integrity: string;
+	export const NODE: string;
+	export const SSH_AUTH_SOCK: string;
+	export const CREDENTIALS_DIRECTORY: string;
+	export const MEMORY_PRESSURE_WRITE: string;
+	export const COLOR: string;
+	export const npm_config_local_prefix: string;
+	export const LIBVA_DRIVER_NAME: string;
+	export const SSH_AGENT_PID: string;
+	export const HL_INITIAL_WORKSPACE_TOKEN: string;
+	export const KITTY_PID: string;
+	export const npm_config_globalconfig: string;
+	export const XCURSOR_SIZE: string;
+	export const EDITOR: string;
+	export const XDG_SEAT: string;
+	export const PWD: string;
+	export const XDG_SESSION_DESKTOP: string;
+	export const LOGNAME: string;
+	export const QT_QPA_PLATFORMTHEME: string;
+	export const XDG_SESSION_TYPE: string;
+	export const CONDA_PREFIX: string;
+	export const npm_package_dev: string;
+	export const npm_config_init_module: string;
+	export const SYSTEMD_EXEC_PID: string;
+	export const KITTY_PUBLIC_KEY: string;
+	export const MOTD_SHOWN: string;
+	export const HOME: string;
+	export const npm_package_peer: string;
+	export const LANG: string;
+	export const _JAVA_AWT_WM_NONREPARENTING: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const npm_package_version: string;
+	export const MEMORY_PRESSURE_WATCH: string;
+	export const WAYLAND_DISPLAY: string;
+	export const npm_package_resolved: string;
+	export const KITTY_WINDOW_ID: string;
+	export const CONDA_PROMPT_MODIFIER: string;
+	export const INVOCATION_ID: string;
+	export const SSH_ENV: string;
+	export const INIT_CWD: string;
+	export const npm_lifecycle_script: string;
+	export const npm_package_optional: string;
+	export const npm_config_npm_version: string;
+	export const XDG_SESSION_CLASS: string;
+	export const TERM: string;
+	export const TERMINFO: string;
+	export const npm_package_name: string;
+	export const _CE_CONDA: string;
+	export const npm_config_prefix: string;
+	export const USER: string;
+	export const CONDA_SHLVL: string;
+	export const HYPRLAND_INSTANCE_SIGNATURE: string;
+	export const DISPLAY: string;
+	export const npm_lifecycle_event: string;
+	export const SHLVL: string;
+	export const MOZ_ENABLE_WAYLAND: string;
+	export const XDG_VTNR: string;
+	export const XDG_SESSION_ID: string;
+	export const npm_config_user_agent: string;
+	export const npm_execpath: string;
+	export const CONDA_PYTHON_EXE: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const CONDA_DEFAULT_ENV: string;
+	export const npm_package_json: string;
+	export const npm_config_noproxy: string;
+	export const PATH: string;
+	export const __GLX_VENDOR_LIBRARY_NAME: string;
+	export const npm_config_node_gyp: string;
+	export const GBM_BACKEND: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const npm_config_global_prefix: string;
+	export const MAIL: string;
+	export const KITTY_INSTALLATION_DIR: string;
+	export const npm_node_execpath: string;
+	export const npm_config_engine_strict: string;
+	export const npm_package_engines_node: string;
+	export const _: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		SHELL: string;
+		npm_command: string;
+		npm_config_userconfig: string;
+		COLORTERM: string;
+		HYPRLAND_CMD: string;
+		npm_config_cache: string;
+		npm_package_dev_optional: string;
+		WLR_NO_HARDWARE_CURSORS: string;
+		CONDA_EXE: string;
+		_CE_M: string;
+		XDG_BACKEND: string;
+		npm_package_integrity: string;
+		NODE: string;
+		SSH_AUTH_SOCK: string;
+		CREDENTIALS_DIRECTORY: string;
+		MEMORY_PRESSURE_WRITE: string;
+		COLOR: string;
+		npm_config_local_prefix: string;
+		LIBVA_DRIVER_NAME: string;
+		SSH_AGENT_PID: string;
+		HL_INITIAL_WORKSPACE_TOKEN: string;
+		KITTY_PID: string;
+		npm_config_globalconfig: string;
+		XCURSOR_SIZE: string;
+		EDITOR: string;
+		XDG_SEAT: string;
+		PWD: string;
+		XDG_SESSION_DESKTOP: string;
+		LOGNAME: string;
+		QT_QPA_PLATFORMTHEME: string;
+		XDG_SESSION_TYPE: string;
+		CONDA_PREFIX: string;
+		npm_package_dev: string;
+		npm_config_init_module: string;
+		SYSTEMD_EXEC_PID: string;
+		KITTY_PUBLIC_KEY: string;
+		MOTD_SHOWN: string;
+		HOME: string;
+		npm_package_peer: string;
+		LANG: string;
+		_JAVA_AWT_WM_NONREPARENTING: string;
+		XDG_CURRENT_DESKTOP: string;
+		npm_package_version: string;
+		MEMORY_PRESSURE_WATCH: string;
+		WAYLAND_DISPLAY: string;
+		npm_package_resolved: string;
+		KITTY_WINDOW_ID: string;
+		CONDA_PROMPT_MODIFIER: string;
+		INVOCATION_ID: string;
+		SSH_ENV: string;
+		INIT_CWD: string;
+		npm_lifecycle_script: string;
+		npm_package_optional: string;
+		npm_config_npm_version: string;
+		XDG_SESSION_CLASS: string;
+		TERM: string;
+		TERMINFO: string;
+		npm_package_name: string;
+		_CE_CONDA: string;
+		npm_config_prefix: string;
+		USER: string;
+		CONDA_SHLVL: string;
+		HYPRLAND_INSTANCE_SIGNATURE: string;
+		DISPLAY: string;
+		npm_lifecycle_event: string;
+		SHLVL: string;
+		MOZ_ENABLE_WAYLAND: string;
+		XDG_VTNR: string;
+		XDG_SESSION_ID: string;
+		npm_config_user_agent: string;
+		npm_execpath: string;
+		CONDA_PYTHON_EXE: string;
+		XDG_RUNTIME_DIR: string;
+		CONDA_DEFAULT_ENV: string;
+		npm_package_json: string;
+		npm_config_noproxy: string;
+		PATH: string;
+		__GLX_VENDOR_LIBRARY_NAME: string;
+		npm_config_node_gyp: string;
+		GBM_BACKEND: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		npm_config_global_prefix: string;
+		MAIL: string;
+		KITTY_INSTALLATION_DIR: string;
+		npm_node_execpath: string;
+		npm_config_engine_strict: string;
+		npm_package_engines_node: string;
+		_: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
